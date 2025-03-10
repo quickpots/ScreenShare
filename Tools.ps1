ba01bf893061f4848ce3b87581eccf2de6baa564
QuickPots <102370841+quickpots@users.noreply.github.com>
2025-03-10 22:14:30 +0000

    Update Tools.ps1

--- Tools.ps1
+++ Tools.ps1
@@ -4,5 +4,3 @@ New-Item -ItemType Directory -Path $folder -Force
 $zipPath = "$folder\SS.zip"
 Invoke-WebRequest -Uri "https://quickpots.fun/screenshare/SS.zip" -OutFile $zipPath
 Start-Process $folder
-$del = [System.Environment]::GetFolderPath("Desktop")
-Remove-Item -path $del\SS.zip"
