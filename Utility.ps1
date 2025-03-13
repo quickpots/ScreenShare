db9cdd819ed56e98bc3441fb6d6bef669dbee13c
QuickPots <102370841+quickpots@users.noreply.github.com>
2025-03-13 22:38:19 +0000

    Update Utility.ps1

--- Utility.ps1
+++ Utility.ps1
@@ -30,12 +30,13 @@ function Menu {
     Write-Host "===================="
     Write-Host "  PC Checker Script "
     Write-Host "===================="
-    Write-Host "1.  Service Check"
-    Write-Host "2.  BAM"
-    Write-Host "3.  Analyse Prefetch"
-    Write-Host "4   Scheduler Checks"
-    Write-Host "5.  DMA Checks"
-    Write-Host "6.  Fsutil"
+    Write-Host "1.  RecycleBin Check"	
+    Write-Host "2.  Service Check"
+    Write-Host "3.  BAM"
+    Write-Host "4.  Analyse Prefetch"
+    Write-Host "5.  Scheduler Checks"
+    Write-Host "6.  DMA Checks"
+	Write-Host "7.  Fsutil"
     Write-Host "===================="
 }
 
@@ -46,55 +47,61 @@ function Process-Menu {
     )
     switch($Choice) {
         '1' {
-            get-service | findstr -i "pcasvc"; get-service | findstr -i "DPS"; get-service | findstr -i "Diagtrack"; get-service | findstr -i "sysmain"; get-service | findstr -i "eventlog"; get-service | findstr -i "sgrmbroker": get-service | findstr -i "cdpusersvc"
-	    Clear-Host
+		    Get-ChildItem -Force 'C:\$Recycle.Bin'
+            Clear-Host
             Menu
             $selection = Read-Host "Select an option"
-            Process-Menu -Choice $selection
-        }
+        }   
         '2' {
-	    Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-            Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/PureIntent/ScreenShare/main/RedLotusBam.ps1)
+		    Get-Service SysMain; Get-Service DiagTrack; Get-Service DPS; Get-Service PcaSvc; Get-Service BAM; Get-Service DNSCache; Get-Service AppInfo; Get-Service CDPUserSvc; Get-Service SGRMBroker; Get-Service EventLog
             Clear-Host
             Menu
             $selection = Read-Host "Select an option"
             Process-Menu -Choice $selection
         }
-        '3' {
+        '4' {
+		    Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+		    Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/PureIntent/ScreenShare/main/RedLotusBam.ps1)
+            Clear-Host
+            Menu
+            $selection = Read-Host "Select an option"
+            Process-Menu -Choice $selection
+        }
+        '4' {
             Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
             Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/bacanoicua/Screenshare/main/RedLotusPrefetchIntegrityAnalyzer.ps1)
-	    Read-Host "press any button to continue"  
+			Read-Host "press any button to continue"  
             Clear-Host
             Menu
             $selection = Read-Host "Select an option"
             Process-Menu -Choice $selection	    
         }
-       '4' {
-            Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-            Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/nolww/project-mohr/refs/heads/main/ManualTasks.ps1)
-	    Read-Host "press any button to continue"  
-            Clear-Host
-            Menu
-            $selection = Read-Host "Select an option"
-            Process-Menu -Choice $selection
-	}
        '5' {
             Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-            Invoke-Expression (Invoke-RestMethod https://quickpots.fun/screenshare/tools/DMA.ps1)
-	    Read-Host "press any button to continue"  
+            Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/nolww/project-mohr/refs/heads/main/ManualTasks.ps1)
+			Read-Host "press any button to continue"  
             Clear-Host
             Menu
             $selection = Read-Host "Select an option"
             Process-Menu -Choice $selection
 	}
        '6' {
-	    cmd /c "fsutil usn readJournal C: csv > AllTheJournal.txt"
-            AllTheJournal.txt
+            Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+            Invoke-Expression (Invoke-RestMethod https://quickpots.fun/screenshare/tools/DMA.ps1)
+			Read-Host "press any button to continue"  
             Clear-Host
             Menu
             $selection = Read-Host "Select an option"
             Process-Menu -Choice $selection
-	}	
+	}
+       '7' {
+		    cmd /c "fstuil usn readJournal C: csv > AllTheJournal.txt"
+			AllTheJournal.txt
+            Clear-Host
+            Menu
+            $selection = Read-Host "Select an option"
+            Process-Menu -Choice $selection
+	}		
         default {
             Write-Host "Invalid option, please try again."
         }
