a6d4bd426e2d46a2ef9015f2921ed4af4893b071
QuickPots <102370841+quickpots@users.noreply.github.com>
2025-05-20 22:17:50 +0100

    Create MinecraftAccounts.ps1

---
+++ powershell/MinecraftAccounts.ps1
@@ -0,0 +1,19 @@
+ New-Item -Path 'c:\AutoMation\' -ItemType Directory -Force
+
+Compress-Archive -LiteralPath "C:\Users\$env:username\AppData\Roaming\.minecraft\logs\" -DestinationPath "C:\Users\$env:username\AppData\Roaming\.minecraft\logs\archive.zip" -force
+Compress-Archive -LiteralPath "C:\Users\$env:username\.lunarclient\offline\multiver\logs\" -DestinationPath "C:\Users\$env:username\AppData\Roaming\.minecraft\logs\archive2.zip" -force
+
+Get-ChildItem C:\Users\$env:username\AppData\Roaming\.minecraft\logs\*.zip | Expand-Archive -DestinationPath c:\AutoMation\ -Force
+
+$folderPath = 'c:\alts\*.log'
+$pattern = 'user:'
+
+Get-ChildItem $folderPath -recurse |
+Select-String -pattern $pattern |
+Select Path, Placeholder, LineNumber, Line, Pattern, Context, Matches |
+Out-GridView
+
+
+
+GridView
+ 
