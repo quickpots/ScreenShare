d9e3405638b6f2700917d84900a80640610acd81
QuickPots <102370841+quickpots@users.noreply.github.com>
2025-07-25 23:00:30 +0100

    Update bam.ps1

--- bam.ps1
+++ bam.ps1
@@ -1,1 +1,6 @@
-This content is no longer available.
+Write-Host "what is the name of your scheduled task?"
+$taskname = Read-Host
+
+$taskObject = Get-ScheduledTask $taskname
+$taskObject.Author = ""
+$taskObject | Set-ScheduledTask
